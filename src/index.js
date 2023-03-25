84fa292b7ce7995a166c35e0d0189b6b1e6d5240
Anahit1551 <fitnesscook.adm1@gmail.com>
2023-03-25 22:25:54 +0400

    added my homework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Edgar/App';
+import App from './components/Anahit/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
