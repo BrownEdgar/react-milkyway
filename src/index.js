90f556c2696cfc021416f14b37f9f78f50e6de55
ASUS <heghine.aghajanyan.gk@gmail.com>
2023-03-28 22:12:22 +0400

    added some items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Anahit/App';
+import App from './components/Heghine/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
