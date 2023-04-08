71e39a7fc1a1128fb7e809e6cf429be3519f53d4
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-08 22:18:39 +0400

    added formilk part two

--- src/components/Edgar/axios/App.jsx
+++ src/components/Edgar/axios/App.jsx
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function App() {
-
+const [data, setData] = useState([])
 
 	useEffect(() => {
 		async function getData() {
-			const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-			console.log(response.data)
+			const response = await axios.get('https://jsonplaceholder.typicode.com/users', {
+				params: {
+					_limit: 2,
+					_start: 5
+				}
+			});
+			setData(response.data)
+		
 		}
 		getData()
 	})
 	return (
 		<div>
-			
+			<pre>
+				{JSON.stringify(data, null, 1)}
+			</pre>
 		</div>
 	)
 }
