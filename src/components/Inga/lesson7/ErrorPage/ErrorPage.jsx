aef7094737e16259a463a8fd3dd25310c589efd9
ingaAslanyan <aslanyaninga44@gmail.com>
2023-04-29 21:16:49 +0400

    new

--- src/components/Inga/lesson7/ErrorPage/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function () {
-  return (
-    <div>
-      <img src="./images/error.jpg" alt="not found" />
-    </div>
-  )
-}
--- src/components/Inga/useReducer/reducer.js
+++
@@ -1,9 +0,0 @@
-export const reducer = (state,action) => {
-    if(action.type === "add-count") {
-        return state + 1
-    }
-    if(action.type === "minus-count") {
-        return state - 1
-    }
-    return state;
-}
