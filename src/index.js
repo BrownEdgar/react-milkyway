d094b68b8e68ddc80c4a8ffdc10f223f6d65ece1
BrownEdgar <nikogosjanedgar@gmail.com>
2023-05-27 22:22:23 +0400

    added Todos example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Edgar/Krknutyun/App'
+import App from './components/Edgar/Todos/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
