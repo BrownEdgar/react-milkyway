99fc1ceedcce33cdf111cb0d8a8194a142ce6ca3
BrownEdgar <nikogosjanedgar@gmail.com>
2023-05-28 01:07:47 +0400

    fix JSON bug

--- src/components/Edgar/Todos/App.jsx
+++ src/components/Edgar/Todos/App.jsx
@@ -4,49 +4,48 @@ import Students from './Students'
 import moment from 'moment'
 
 import './App.scss'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 export default function App() {
-	const [students, setStudents] = useState(JSON.parse(localStorage.getItem('students') || '[]'))
+  const [students, setStudents] = useLocalStorage('students')
+  console.log('students:', students);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      firstname,
+      lastname,
+      email,
+      languages } = e.target;
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const {
-			firstname,
-			lastname,
-			email,
-			languages } = e.target;
+    const student = {
+      id: Date.now(),
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      languages: languages.value,
+      registeredDate: moment()
+    }
+    const result = [...students, student];
+    localStorage.setItem('students', JSON.stringify(result))
+    setStudents(result)
 
-		const student = {
-			id: Date.now(),
-			firstname: firstname.value,
-			lastname: lastname.value,
-			email: email.value,
-			languages: languages.value,
-			registeredDate: moment()
-		}
-		setStudents(prevStudents => {
-			return [...prevStudents, student]
-		})
-		
-		
-		e.target.reset()
-	}
-	useEffect(() => {
-		localStorage.setItem('students', JSON.stringify(students))
-	}, [students])
-	
 
-	return (
-		<div className='container'>
-			<div className="container-info">
+    e.target.reset()
+  }
 
-			</div>
-			<div className='container-form'>
-				<Form onSubmit={handleSubmit} />
-			</div>
-			<div className='container-students'>
-				<Students students={students} />
-			</div>
-		</div>
-	)
+
+
+  return (
+    <div className='container'>
+      <div className="container-info">
+
+      </div>
+      <div className='container-form'>
+        <Form onSubmit={handleSubmit} />
+      </div>
+      <div className='container-students'>
+        <Students students={students} />
+      </div>
+    </div>
+  )
 }
